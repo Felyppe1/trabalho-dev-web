c40dcd59e6f3ea76a64d5c9e99ce3edc0b1aa6ff
Thiago Araujo <thiagoaraujo43034@gmail.com>
2025-07-09 01:11:07 +0000

    feat: Data access obeject for Transactions

---
+++ src/main/java/com/trabalho/devweb/infrastructure/repositories/TransactionRepository.java
@@ -0,0 +1,44 @@
+package com.trabalho.devweb.infrastructure.repositories;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.postgresql.core.ConnectionFactory;
+import com.trabalho.devweb.infrastructure.databaseconnection.PostgresConnection;
+import com.trabalho.devweb.domain.Transaction;
+
+public class TransactionRepository{
+    
+
+    public List<Transaction> findByAccountId(String accountId){
+        List<Transaction> list = new ArrayList<>();
+
+        String searchTrasaction = "SELECT * FROM transaction WHERE origin_id = ? OR target_id = ? ORDER BY created_at DESC";
+        try(Connection conn = PostgresConnection.getConnection()){
+            PreparedStatement statement = conn.prepareStatement(searchTrasaction);
+            statement.setString(1, accountId);
+            statement.setString(2, accountId);
+            
+            ResultSet result = statement.executeQuery();  
+            
+            while (result.next()) {
+                Transaction transactions = new Transaction();
+                transactions.setOriginId(result.getString("oringin_id"));
+                transactions.setTargetId(result.getString("target_id"));
+                transactions.setCreatedAt(result.getTimestamp("created_at").toLocalDateTime());
+                transactions.setType(result.getString("type"));
+                transactions.setAmount(result.getBigDecimal("amount"));
+                transactions.setDescription(result.getString("description"));
+                list.add(transactions);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+    
+}
