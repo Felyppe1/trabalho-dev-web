3856e077e63026ab31829820c6001941519e2327
Thiago Araujo <thiagoaraujo43034@gmail.com>
2025-07-09 01:36:27 +0000

    feat: controller for requests of statement

--- src/main/java/com/trabalho/devweb/infrastructure/controllers/StatementController.java
+++ src/main/java/com/trabalho/devweb/infrastructure/controllers/StatementController.java
@@ -1,20 +1,35 @@
 package com.trabalho.devweb.infrastructure.controllers;
 
 import java.io.IOException;
+import java.util.List;
+
+import com.trabalho.devweb.domain.Account;
+import com.trabalho.devweb.domain.Transaction;
+import com.trabalho.devweb.infrastructure.repositories.TransactionRepository;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebServlet(name = "StatementController", urlPatterns = "/extrato")
 public class StatementController extends HttpServlet {
+    private TransactionRepository repository = new TransactionRepository();
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/statement.jsp");
-        dispatcher.forward(request, response);
-        // response.setContentType("text/html");
-        // response.getWriter().println("<h1>Hello, World! Servlet funcionando!</h1>");
+
+        Account account = (Account) request.getSession().getAttribute("account");
+
+        if (account == null) {
+            response.sendRedirect("login.jsp");
+            return;
+        }
+        List<Transaction> transactions = repository.findByAccountId(account.getId());
+        request.setAttribute("transaction", transactions);
+        request.getRequestDispatcher("/statement.jsp").forward(request, response);               
     }
 }
