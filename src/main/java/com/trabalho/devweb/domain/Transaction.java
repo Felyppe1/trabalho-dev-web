9ba1064509f0b54f5afab2c5cd22a7ae514dc471
Thiago Araujo <thiagoaraujo43034@gmail.com>
2025-07-09 00:02:41 +0000

    feat: created transaction model

---
+++ src/main/java/com/trabalho/devweb/domain/Transaction.java
@@ -0,0 +1,72 @@
+package com.trabalho.devweb.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Transaction {
+    private String originId;
+    private String targetId;
+    private LocalDateTime createdAt;
+    private String type;
+    private BigDecimal amount;
+    private String description;
+    private BigDecimal balanceAfter;
+
+    public Transaction() {}
+
+    public String getOriginId() {
+        return originId;
+    }
+
+    public void setOriginId(String originId) {
+        this.originId = originId;
+    }
+
+    public String getTargetId() {
+        return targetId;
+    }
+
+    public void setTargetId(String targetId) {
+        this.targetId = targetId;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getBalanceAfter() {
+        return balanceAfter;
+    }
+
+    public void setBalanceAfter(BigDecimal balanceAfter) {
+        this.balanceAfter = balanceAfter;
+    }
+}
