5708245e4283d122981505912aa062663550f54d
Thiago Araujo <thiagoaraujo43034@gmail.com>
2025-07-10 06:22:57 +0000

    fix:duplicate class name error

---
+++ src/main/java/com/trabalho/devweb/infrastructure/controllers/StatementController.java
@@ -0,0 +1,34 @@
+package com.trabalho.devweb.infrastructure.controllers;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.trabalho.devweb.domain.Account;
+import com.trabalho.devweb.domain.Transaction;
+import com.trabalho.devweb.infrastructure.repositories.TransactionRepository;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet(name = "StatementController", urlPatterns = "/extrato")
+public class StatementController extends HttpServlet {
+   
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        Account account = (Account) request.getSession().getAttribute("account");
+
+        if (account == null) {
+            response.sendRedirect("login.jsp");
+            return;
+        }
+        
+        request.getRequestDispatcher("/statement.jsp").forward(request, response);               
+    }
+}
