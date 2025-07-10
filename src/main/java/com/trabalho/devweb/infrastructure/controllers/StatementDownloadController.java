549417465d4aa179351f470a9e3db231615800d8
Thiago Araujo <thiagoaraujo43034@gmail.com>
2025-07-10 22:31:00 +0000

    fix: Error 404 in mapping of servlet extrato/view

--- src/main/java/com/trabalho/devweb/infrastructure/controllers/StatementDownloadController.java
+++ src/main/java/com/trabalho/devweb/infrastructure/controllers/StatementDownloadController.java
@@ -5,9 +5,10 @@ import java.io.OutputStream;
 import java.sql.Connection;
 import java.util.List;
 
-import javax.servlet.annotation.WebServlet;
+import jakarta.servlet.annotation.WebServlet;
 
 import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
 import com.lowagie.text.Paragraph;
 import com.lowagie.text.pdf.PdfWriter;
 import com.trabalho.devweb.domain.Account;
@@ -20,50 +21,60 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/extrato/download")
+@WebServlet(name = "StatementDownloadController", urlPatterns = "/extrato/download")
 public class StatementDownloadController extends HttpServlet{
-    
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+    throws ServletException, IOException {
         Account account = (Account) request.getSession().getAttribute("account");
 
         int month = Integer.parseInt(request.getParameter("month"));
         int year = Integer.parseInt(request.getParameter("year"));
 
+        
+        System.out.println("> Mês: " + month);
+        System.out.println("> Ano: " + year);
 
-
-        try {
-            Connection connection = PostgresConnection.getConnection();
+        try 
+            (Connection connection = PostgresConnection.getConnection();){
             TransactionRepository transactionRepository = new TransactionRepository(connection);
             List<Transaction> transactions = transactionRepository.findByAccountIdAndMonth(account.getId(), month, year);
 
             response.setContentType("application/pdf");
-            response.setHeader("Content-Disposition", "attachment; filename=extrato_" + month + " " + year + ".pdf");
+            response.setHeader("Content-Disposition", "inline; filename=extrato_" + month + "_" + year + ".pdf");
 
             OutputStream out = response.getOutputStream();
             generatePDF(transactions, out, month, year, account.getName());
-        } catch (Exception e) {
+            }
+            
+         catch (Exception e) {
             e.printStackTrace();
             response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Erro ao gerar PDF");
         }
 
 
     }
-        private void generatePDF(List<Transaction> transactions, OutputStream out, int month, int year, String userName) throws Exception{
-        Document document = new Document();
-        PdfWriter.getInstance(document, out);
-        document.open();        
+    private void generatePDF(List<Transaction> transactions, OutputStream out, int month, int year, String userName) throws Exception{
+        
+        try{
+            Document document = new Document();
+            PdfWriter.getInstance(document, out);
+            document.open();        
 
-        document.add(new Paragraph("Extrato de " + userName));
-        document.add(new Paragraph("Mês: " + month + " de " + year));
-        document.add(new Paragraph(" "));
+            document.add(new Paragraph("Extrato de " + userName));
+            document.add(new Paragraph("Mês: " + month + " de " + year));
+            document.add(new Paragraph(" "));
 
-        for(Transaction t : transactions){
-            document.add(new Paragraph(
-                t.getCreatedAt() + " - " + t.getType() + " - R$ " + t.getAmount() + " - Saldo: R$ " + t.getBalanceAfter()
-            ));
+            for(Transaction t : transactions){
+                document.add(new Paragraph(
+                    t.getCreatedAt() + " - " + t.getType() + " - R$ " + t.getAmount() + " - Saldo: R$ " + t.getBalanceAfter()
+                ));
+            }
+            document.close();
+        }catch(DocumentException e){
+            e.printStackTrace();
+            //response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Erro ao gerar o PDF.");
         }
-        document.close();
     }
     
 }
